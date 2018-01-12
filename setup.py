36ba7439e531828c4e1ad5d29834df05fcb20848
farhan-cashify <farhan.k@cashify.in>
2018-01-12 12:52:32 +0530

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,38 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-inspectdb-refactor',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    license='BSD License',  # example license
+    description='A simple Django app to create refactor output of inspectdb command into separate classes.',
+    long_description=README,
+    url='https://github.com/farhan0581/django-inspectdb-refactor',
+    author='Farhan Khan',
+    author_email='farhan0581@gmail.com',
+    install_requires=[
+        'django>=1.9',
+    ]
+    classifiers=[
+        'Development Status :: 1 - Beta',
+        'Framework :: Django',
+        'Framework :: Django :: 1.11',  # replace "X.Y" as appropriate
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',  # example license
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
